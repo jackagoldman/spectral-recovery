8f89ffb656d2d4679c1b5806690ddee9642208b6
jackagoldman <jandrewgoldman@gmail.com>
2024-04-12 09:57:51 -0400

    add water mask

--- py/img_processing_tools.py
+++ py/img_processing_tools.py
@@ -6,29 +6,27 @@ def check_image_info(img):
 def ls8_Indices(lsImage):
   nbr = lsImage.normalizedDifference(['SR_B5', 'SR_B7']).toFloat()
   qa = lsImage.select(['QA_PIXEL'])
-  ndsi = image.normalizedDifference({'SR_B3', 'SR_B6'}).toFloat()
-  nbr = nbr.addBands(ndsi)
-  return nbr.addBands([qa]).select([0,1,2], ['nbr', 'ndsi','QA_PIXEL']).copyProperties(lsImage, ['system:time_start'])
+  return nbr.addBands([qa]).select([0,1], ['nbr', 'QA_PIXEL']).copyProperties(lsImage, ['system:time_start'])
 
   
 #// Returns vegetation indices for LS4, LS5 and LS7 . change to CO2_T2_L2
- def ls4_7_Indices(lsImage):
-   nbr = lsImage.normalizedDifference(['SR_B4', 'SR_B7']).toFloat()
-   ndsi = image.normalizedDifference({'SR_B2', 'SR_B5'}).toFloat()
-   nbr = nbr.addBands(ndsi)
-   qa = lsImage.select(['QA_PIXEL'])
-   return nbr.addBands([qa]).select([0,1,2], ['nbr','ndsi', 'QA_PIXEL']).copyProperties(lsImage, ['system:time_start'])
+def ls4_7_Indices(lsImage):
+  nbr = lsImage.normalizedDifference(['SR_B4', 'SR_B7']).toFloat()
+  qa = lsImage.select(['QA_PIXEL'])
+  return nbr.addBands([qa]).select([0,1], ['nbr', 'QA_PIXEL']).copyProperties(lsImage, ['system:time_start'])
   
  
 # Mask Landsat surface reflectance images
 # Creates a mask for clear pixels 
- def lsCfmask(lsImg):
+def lsCfmask(lsImg):
   qa = lsImg.select("QA_PIXEL")
   cloudBitMask = 1 << 3
   cirrusBitMask = 1 << 4
   waterBitMask = 1 << 7
+  snowBitMask = 1 << 5
   mask = qa.bitwiseAnd(cloudBitMask).eq(0).And(qa.bitwiseAnd(cirrusBitMask).eq(0)).And(
-    qa.bitwiseAnd(waterBitMask).eq(0))
+    qa.bitwiseAnd(waterBitMask).eq(0)).And(
+    qa.bitwiseAnd(snowBitMask).eq(0))
   return lsImg.updateMask(mask) \
       .select("nbr") \
       .copyProperties(lsImg, ["system:time_start"])
@@ -71,40 +69,10 @@ def get_keys(img, geom):
   return(res.getInfo())
 
 
-# mask imagery
-def create_mask_from_shapefile(input_shapefile, raster_path, out_path):
-"""
-this function crops a raster based on a feature in a shapefile which matches the id
-takes three arguments:
-input_shapefile
-raster_path
-out_path
-"""
-#first loop through dir
-    with os.scandir(raster_path) as p: # Open the folder containing the rasters as p.
-        for raster in p: # Get each raster in the folder.
-          with rasterio.open(raster, 'r') as src:
-            name = src.name
-            name = name.replace('/Users/jgoldman/Google Drive/My Drive/winter_test_folder/', '')
-            name = name.replace('.tif', "")
-            
-            # read in shapefile
-            with fiona.open(input_shapefile) as shp:
-              for feature in shp:
-                  if feature['properties']['Fire_ID'] in name:
-                    shape = [feature['geometry']]
-                    out_image, out_transform = mask(src, shape, crop=True) # setting all pixels outside of the feature zone to zero
-                    out_meta = src.meta
-        
-                    out_meta.update({"driver": "GTiff",
-                    "height": out_image.shape[1],
-                    "width": out_image.shape[2],
-                    "transform": out_transform})
-            
-                   # write output name
-                    output_file = name + '.tif'
-                
-                    with rasterio.open(os.path.join(out_path, output_file), "w", **out_meta) as dest:
-                        dest.write(out_image)
-                    
+def water_mask(img):
+  gsw = ee.Image("JRC/GSW1_1/GlobalSurfaceWater")
+  water = gsw.select('max_extent')
+  mask = water.eq(0)
+  maskedImg = img.updateMask(mask)
+  return(maskedImg)
 
