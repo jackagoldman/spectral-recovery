b332628b7f3600ab12470f0e0304595d2f1ea5ff
jackagoldman <jandrewgoldman@gmail.com>
2024-04-13 14:03:15 -0400

    add altered geemap timeseries function

---
+++ landsat_timeseries_legacy.py
@@ -0,0 +1,338 @@
+import ee
+import geemap
+import datetime
+
+
+def landsat_timeseries_legacy(
+    roi=None,
+    start_year=1982,
+    end_year=2023,
+    start_date="06-10",
+    end_date="09-20",
+    apply_fmask=True,
+    frequency="year",
+    date_format=None,
+):
+    """Generates an annual Landsat ImageCollection. This algorithm is adapted from https://gist.github.com/jdbcode/76b9ac49faf51627ebd3ff988e10adbc. A huge thank you to Justin Braaten for sharing his fantastic work.
+    Args:
+        roi (object, optional): Region of interest to create the timelapse. Defaults to None.
+        start_year (int, optional): Starting year for the timelapse. Defaults to 1984.
+        end_year (int, optional): Ending year for the timelapse. Defaults to 2021.
+        start_date (str, optional): Starting date (month-day) each year for filtering ImageCollection. Defaults to '06-10'.
+        end_date (str, optional): Ending date (month-day) each year for filtering ImageCollection. Defaults to '09-20'.
+        apply_fmask (bool, optional): Whether to apply Fmask (Function of mask) for automated clouds, cloud shadows, snow, and water masking.
+        frequency (str, optional): Frequency of the timelapse. Defaults to 'year'.
+        date_format (str, optional): Format of the date. Defaults to None.
+    Returns:
+        object: Returns an ImageCollection containing annual Landsat images.
+    """
+
+    ################################################################################
+    # Input and output parameters.
+    import re
+
+    import datetime
+
+    if roi is None:
+        roi = ee.Geometry.Polygon(
+            [
+                [
+                    [-115.471773, 35.892718],
+                    [-115.471773, 36.409454],
+                    [-114.271283, 36.409454],
+                    [-114.271283, 35.892718],
+                    [-115.471773, 35.892718],
+                ]
+            ],
+            None,
+            False,
+        )
+
+    if not isinstance(roi, ee.Geometry):
+        try:
+            roi = roi.geometry()
+        except Exception as e:
+            print("Could not convert the provided roi to ee.Geometry")
+            print(e)
+            return
+
+    feq_dict = {
+        "year": "YYYY",
+        "month": "YYYY-MM",
+        "quarter": "YYYY-MM",
+    }
+
+    if date_format is None:
+        date_format = feq_dict[frequency]
+
+    if frequency not in feq_dict:
+        raise ValueError("frequency must be year, quarter, or month.")
+
+    ################################################################################
+
+    # Setup vars to get dates.
+    if isinstance(start_year, int) and (start_year >= 1982) and (start_year < 2023):
+        pass
+    else:
+        print("The start year must be an integer >= 1984.")
+        return
+
+    if isinstance(end_year, int) and (end_year > 1982) and (end_year <= 2023):
+        pass
+    else:
+        print("The end year must be an integer <= 2021.")
+        return
+
+    if re.match(r"[0-9]{2}-[0-9]{2}", start_date) and re.match(
+        r"[0-9]{2}-[0-9]{2}", end_date
+    ):
+        pass
+    else:
+        print("The start date and end date must be month-day, such as 06-10, 09-20")
+        return
+
+    try:
+        datetime.datetime(int(start_year), int(start_date[:2]), int(start_date[3:5]))
+        datetime.datetime(int(end_year), int(end_date[:2]), int(end_date[3:5]))
+    except Exception as e:
+        print("The input dates are invalid.")
+        raise Exception(e)
+
+    def days_between(d1, d2):
+        d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
+        d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+        return abs((d2 - d1).days)
+
+    n_days = days_between(
+        str(start_year) + "-" + start_date, str(start_year) + "-" + end_date
+    )
+    start_month = int(start_date[:2])
+    start_day = int(start_date[3:5])
+    # start_date = str(start_year) + "-" + start_date
+    # end_date = str(end_year) + "-" + end_date
+
+    # # Define a collection filter by date, bounds, and quality.
+    # def colFilter(col, aoi):  # , startDate, endDate):
+    #     return(col.filterBounds(aoi))
+
+    # Landsat collection preprocessingEnabled
+    # Get Landsat surface reflectance collections for OLI, ETM+ and TM sensors.
+    LC08col = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")
+    LE07col = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR")
+    LT05col = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR")
+    LT04col = ee.ImageCollection("LANDSAT/LT04/C01/T1_SR")
+
+    # Define a collection filter by date, bounds, and quality.
+    def colFilter(col, roi, start_date, end_date):
+        return col.filterBounds(roi).filterDate(start_date, end_date)
+        # .filter('CLOUD_COVER < 5')
+        # .filter('GEOMETRIC_RMSE_MODEL < 15')
+        # .filter('IMAGE_QUALITY == 9 || IMAGE_QUALITY_OLI == 9'))
+
+    # Function to get and rename bands of interest from OLI.
+    def renameOli(img):
+        return img.select(
+            ["B2", "B3", "B4", "B5", "B6", "B7", "pixel_qa"],
+            ["Blue", "Green", "Red", "NIR", "SWIR1", "SWIR2", "pixel_qa"],
+        )
+
+    # Function to get and rename bands of interest from ETM+.
+    def renameEtm(img):
+        return img.select(
+            ["B1", "B2", "B3", "B4", "B5", "B7", "pixel_qa"],
+            ["Blue", "Green", "Red", "NIR", "SWIR1", "SWIR2", "pixel_qa"],
+        )
+
+    # Add NBR for LandTrendr segmentation.
+    def calcNbr(img):
+        return img.addBands(
+            img.normalizedDifference(["NIR", "SWIR2"]).multiply(-10000).rename("NBR")
+        ).int16()
+
+    # Define function to mask out clouds and cloud shadows in images.
+    # Use CFmask band included in USGS Landsat SR image product.
+    def fmask(img):
+        cloudShadowBitMask = 1 << 3
+        cloudsBitMask = 1 << 5
+        qa = img.select("pixel_qa")
+        mask = (
+            qa.bitwiseAnd(cloudShadowBitMask)
+            .eq(0)
+            .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
+        )
+        return img.updateMask(mask)
+
+    # Define function to prepare OLI images.
+    def prepOli(img):
+        orig = img
+        img = renameOli(img)
+        if apply_fmask:
+            img = fmask(img)
+        return ee.Image(img.copyProperties(orig, orig.propertyNames())).resample(
+            "bicubic"
+        )
+
+    # Define function to prepare ETM+ images.
+    def prepEtm(img):
+        orig = img
+        img = renameEtm(img)
+        if apply_fmask:
+            img = fmask(img)
+        return ee.Image(img.copyProperties(orig, orig.propertyNames())).resample(
+            "bicubic"
+        )
+
+    # Get annual median collection.
+    def getAnnualComp(y):
+        startDate = ee.Date.fromYMD(
+            ee.Number(y), ee.Number(start_month), ee.Number(start_day)
+        )
+        endDate = startDate.advance(ee.Number(n_days), "day")
+
+        # Filter collections and prepare them for merging.
+        LC08coly = colFilter(LC08col, roi, startDate, endDate).map(prepOli)
+        LE07coly = colFilter(LE07col, roi, startDate, endDate).map(prepEtm)
+        LT05coly = colFilter(LT05col, roi, startDate, endDate).map(prepEtm)
+        LT04coly = colFilter(LT04col, roi, startDate, endDate).map(prepEtm)
+
+        # Merge the collections.
+        col = LC08coly.merge(LE07coly).merge(LT05coly).merge(LT04coly)
+
+        yearImg = col.median()
+        nBands = yearImg.bandNames().size()
+        yearImg = ee.Image(ee.Algorithms.If(nBands, yearImg, dummyImg))
+        return calcNbr(yearImg).set(
+            {
+                "year": y,
+                "system:time_start": startDate.millis(),
+                "nBands": nBands,
+                "system:date": ee.Date(startDate).format(date_format),
+            }
+        )
+
+    # Get monthly median collection.
+    def getMonthlyComp(startDate):
+        startDate = ee.Date(startDate)
+        endDate = startDate.advance(1, "month")
+
+        # Filter collections and prepare them for merging.
+        LC08coly = colFilter(LC08col, roi, startDate, endDate).map(prepOli)
+        LE07coly = colFilter(LE07col, roi, startDate, endDate).map(prepEtm)
+        LT05coly = colFilter(LT05col, roi, startDate, endDate).map(prepEtm)
+        LT04coly = colFilter(LT04col, roi, startDate, endDate).map(prepEtm)
+
+        # Merge the collections.
+        col = LC08coly.merge(LE07coly).merge(LT05coly).merge(LT04coly)
+
+        monthImg = col.median()
+        nBands = monthImg.bandNames().size()
+        monthImg = ee.Image(ee.Algorithms.If(nBands, monthImg, dummyImg))
+        return calcNbr(monthImg).set(
+            {
+                "system:time_start": startDate.millis(),
+                "nBands": nBands,
+                "system:date": ee.Date(startDate).format(date_format),
+            }
+        )
+
+    # Get quarterly median collection.
+    def getQuarterlyComp(startDate):
+        startDate = ee.Date(startDate)
+
+        endDate = startDate.advance(3, "month")
+
+        # Filter collections and prepare them for merging.
+        LC08coly = colFilter(LC08col, roi, startDate, endDate).map(prepOli)
+        LE07coly = colFilter(LE07col, roi, startDate, endDate).map(prepEtm)
+        LT05coly = colFilter(LT05col, roi, startDate, endDate).map(prepEtm)
+        LT04coly = colFilter(LT04col, roi, startDate, endDate).map(prepEtm)
+
+        # Merge the collections.
+        col = LC08coly.merge(LE07coly).merge(LT05coly).merge(LT04coly)
+
+        quarter = col.median()
+        nBands = quarter.bandNames().size()
+        quarter = ee.Image(ee.Algorithms.If(nBands, quarter, dummyImg))
+        return calcNbr(quarter).set(
+            {
+                "system:time_start": startDate.millis(),
+                "nBands": nBands,
+                "system:date": ee.Date(startDate).format(date_format),
+            }
+        )
+
+    ################################################################################
+
+    # Make a dummy image for missing years.
+    bandNames = ee.List(["Blue", "Green", "Red", "NIR", "SWIR1", "SWIR2", "pixel_qa"])
+    fillerValues = ee.List.repeat(0, bandNames.size())
+    dummyImg = ee.Image.constant(fillerValues).rename(bandNames).selfMask().int16()
+
+    # ################################################################################
+    # # Get a list of years
+    # years = ee.List.sequence(start_year, end_year)
+
+    # ################################################################################
+    # # Make list of annual image composites.
+    # imgList = years.map(getAnnualComp)
+
+    if frequency == "year":
+        years = ee.List.sequence(start_year, end_year)
+        imgList = years.map(getAnnualComp)
+    elif frequency == "quarter":
+        quarters = date_sequence(
+            str(start_year) + "-01-01", str(end_year) + "-12-31", "quarter", date_format
+        )
+        imgList = quarters.map(getQuarterlyComp)
+    elif frequency == "month":
+        months = date_sequence(
+            str(start_year) + "-01-01", str(end_year) + "-12-31", "month", date_format
+        )
+        imgList = months.map(getMonthlyComp)
+
+    # Convert image composite list to collection
+    imgCol = ee.ImageCollection.fromImages(imgList)
+
+    imgCol = imgCol.map(
+        lambda img: img.clip(roi).set({"coordinates": roi.coordinates()})
+    )
+
+    return imgCol
+
+    # ################################################################################
+    # # Run LandTrendr.
+    # lt = ee.Algorithms.TemporalSegmentation.LandTrendr(
+    #     timeSeries=imgCol.select(['NBR', 'SWIR1', 'NIR', 'Green']),
+    #     maxSegments=10,
+    #     spikeThreshold=0.7,
+    #     vertexCountOvershoot=3,
+    #     preventOneYearRecovery=True,
+    #     recoveryThreshold=0.5,
+    #     pvalThreshold=0.05,
+    #     bestModelProportion=0.75,
+    #     minObservationsNeeded=6)
+
+    # ################################################################################
+    # # Get fitted imagery. This starts export tasks.
+    # def getYearStr(year):
+    #     return(ee.String('yr_').cat(ee.Algorithms.String(year).slice(0,4)))
+
+    # yearsStr = years.map(getYearStr)
+
+    # r = lt.select(['SWIR1_fit']).arrayFlatten([yearsStr]).toShort()
+    # g = lt.select(['NIR_fit']).arrayFlatten([yearsStr]).toShort()
+    # b = lt.select(['Green_fit']).arrayFlatten([yearsStr]).toShort()
+
+    # for i, c in zip([r, g, b], ['r', 'g', 'b']):
+    #     descr = 'mamore-river-'+c
+    #     name = 'users/user/'+descr
+    #     print(name)
+    #     task = ee.batch.Export.image.toAsset(
+    #     image=i,
+    #     region=roi.getInfo()['coordinates'],
+    #     assetId=name,
+    #     description=descr,
+    #     scale=30,
+    #     crs='EPSG:3857',
+    #     maxPixels=1e13)
+    #     task.start()
