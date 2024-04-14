38dcb935ff88032551a2c5796ac467fbdef00834
jackagoldman <jandrewgoldman@gmail.com>
2024-04-14 10:17:27 -0400

    adjust workflow using lt-gee-py

--- perfire_landtrendr.py
+++ perfire_landtrendr.py
@@ -8,6 +8,7 @@ import pandas as pd
 import os
 from ltgee import LandTrendr
 from datetime import date
+import ltgee
 
 ee.Initialize()
 # 
@@ -27,7 +28,7 @@ on_qc_fires = on_fires.merge(qc_fires)
 fires = on_qc_fires
 
 
-############### start of landtrendr analysis
+############### START OF LANDTRENDR AND BURN SEVERITY ANALYSIS ###############
 
 # get Fires
 fids = fires.aggregate_array('Fire_ID')
@@ -35,111 +36,142 @@ fids = fires.aggregate_array('Fire_ID')
 fids = fids.getInfo()
 fids1 = str(fids)
 
-test_fire = fires.filter(ee.Filter.eq('Fire_ID', 'FOR13_1987_316'))
+test_fire = fires.filter(ee.Filter.eq('Fire_ID', 'QC_533_1996'))
 ft = test_fire.first()
 # loop through list of fire ids
 
-fireBounds = ft.geometry().bounds()
-fyr = ft.get('Fire_Year')
-fyr = fyr.getInfo()
-fyr = str(fyr)
-fireYear = ee.Date(fyr)
-preFireYear = fireYear.advance(-1, 'year')
-
-col =landsat_timeseries_legacy(
-    roi=fireBounds,
-    start_year= 1982,
-    end_year=2023,
-    start_date="04-01",
-    end_date="11-15",
-    apply_fmask=True,
-    frequency="year",
-    date_format=None,
-)
-
-lt = ee.Algorithms.TemporalSegmentation.LandTrendr(
-       timeSeries=col.select(['NBR', 'SWIR2', 'NIR', 'Green']),
-       maxSegments=10, 
-       spikeThreshold=0.7, 
-       vertexCountOvershoot=3,
-       preventOneYearRecovery=True,
-       recoveryThreshold=0.5,
-       pvalThreshold=0.05,
-       bestModelProportion=0.75,
-       minObservationsNeeded=6)
-
-def getYearStr(year):
-  return(ee.String('yr_').cat(ee.Algorithms.String(year).slice(0,4)))
-
-years = ee.List.sequence(1982, 2023)
+for j in fids:
+  Name = j
+  fiya= fires.filterMetadata('Fire_ID', 'equals', Name).first();
+  ft = ee.Feature(fiya)
+  fName = ft.get("Fire_ID")
+  fire = ft
 
 
-yearsStr = years.map(getYearStr)
+  fireBounds = ft.geometry().bounds()
+  fyr = ft.get('Fire_Year')
+  fyr = fyr.getInfo()
+  fyr = str(fyr)
+  fireYear = ee.Date(fyr)
+  preFireYear = fireYear.advance(-1, 'year')
+  
+  
+  lt_params = {
+      "start_date": date(1984, 4,1),
+      "end_date": date(2021, 11,1),
+      "index": 'NBR',
+      "ftv_list": ['TCB', 'TCG', 'TCW', 'TCA', 'NBR'],
+      "mask_labels": ['cloud', 'shadow', 'snow', 'water'],
+      "area_of_interest": fireBounds 
+      ,"run_params": {
+              "maxSegments": 6,
+              "spikeThreshold": 0.9,
+              "vertexCountOvershoot":  3,
+              "preventOneYearRecovery":  True,
+              "recoveryThreshold":  0.25,
+              "pvalThreshold":  .05,
+              "bestModelProportion":  0.75,
+              "minObservationsNeeded": 2,
+          }}
 
-nir_fit = lt.select(['NIR_fit'])
+  lt = LandTrendr(debug=True, **lt_params)
+  lt.run()
+  
+  #lt.data.getInfo()
+  #lt.sr_collection.getInfo()
+ 
+  #lt.lt_collection.getInfo()
+   
+ # lt.clear_pixel_count_collection
+   
+  ftv_nbr = lt.data.select('ftv_nbr_fit').clip(lt_params['area_of_interest'])
+  ftv_tca = lt.data.select('ftv_tca_fit').clip(lt_params['area_of_interest'])
+  ftv_tcb = lt.data.select('ftv_tcb_fit').clip(lt_params['area_of_interest'])
+  ftv_tcw = lt.data.select('ftv_tcw_fit').clip(lt_params['area_of_interest'])
+  ftv_tcg = lt.data.select('ftv_tcg_fit').clip(lt_params['area_of_interest'])
+  
+  
+  # creat list of years to get imagery
+  years = ee.List.sequence(1984, 2021)
+  
+  # make it a string
+  yearsStr = years.map(getYearStr)
+  
+   ### get RBR image
+  # create sequence for each fire to filter image to years we want
+  start = ee.Date(fireYear.advance(-1, 'year'))
+  start = getYearNumber(start)
+  end = ee.Date(fireYear.advance(11, 'year'))
+  end = getYearNumber(end)
+  yoi = ee.List.sequence(start, end)
+  yoiStr = yoi.map(getYearStr)
+  yoiStr = yoiStr.getInfo()
+ 
+  
+  #nbr
+  ftv_nbr2 = ftv_nbr.arrayFlatten([yearsStr])
+  nbr = ftv_nbr2.select(yoiStr)
+  
+  #tca
+  ftv_tca2 = ftv_tca.arrayFlatten([yearsStr])
+  tca = ftv_tca2.select(yoiStr)
+  
+  #tcb
+  ftv_tcb2 = ftv_tcb.arrayFlatten([yearsStr])
+  tcb = ftv_tcb2.select(yoiStr)
+  
+  #tcw
+  ftv_tcw2 = ftv_tcw.arrayFlatten([yearsStr])
+  tcw = ftv_tcw2.select(yoiStr)
+  
+  #tcg
+  ftv_tcg2 = ftv_tcg.arrayFlatten([yearsStr])
+  tcg = ftv_tcg2.select(yoiStr)
+  
+  
 
-swir2_fit = lt.select(['SWIR2_fit'])
+  ## get nbr
+  preNBR = nbr.select(getYear(yoiStr, 0)).rename('preNBR').toFloat()
+  yof = nbr.select(getYear(yoiStr, 1)).rename('yof')
+  postNBR = nbr.select(getYear(yoiStr, 2)).rename('postNBR').toFloat()
+  nbr2 = nbr.select(getYear(yoiStr, 3)).rename('nbr_2yr')
+  nbr3 = nbr.select(getYear(yoiStr, 4)).rename('nbr_3yr')
+  nbr4 = nbr.select(getYear(yoiStr, 5)).rename('nbr_4yr')
+  nbr5 = nbr.select(getYear(yoiStr, 6)).rename('nbr_5yr')
+  nbr6 = nbr.select(getYear(yoiStr, 7)).rename('nbr_6yr')
+  nbr7 = nbr.select(getYear(yoiStr, 8)).rename('nbr_7yr')
+  nbr8 = nbr.select(getYear(yoiStr, 9)).rename('nbr_8yr')
+  nbr9 = nbr.select(getYear(yoiStr, 10)).rename('nbr_9yr')
+  nbr10 = nbr.select(getYear(yoiStr,11)).rename('nbr_10yr')
 
-NIRftvStack = nir_fit.arrayFlatten([yearsStr])
+  
+  # add prefire and post fire
+  indices = preNBR.addBands(postNBR)
+  
+  # calculate burn severity metrics
+  burnIndices = calcBS(indices, ft)
+  
+  # add nbr bands 
+  nbrBands = nbr2.addBands(nbr3)
+  nbrBands = nbrBands.addBands(nbr4)
+  nbrBands = nbrBands.addBands(nbr5)
+  nbrBands = nbrBands.addBands(nbr6)
+  nbrBands = nbrBands.addBands(nbr7)
+  nbrBands = nbrBands.addBands(nbr8)
+  nbrBands = nbrBands.addBands(nbr9)
+  nbrBands = nbrBands.addBands(nbr10)
+  nbrBands = nbrBands.addBands(yof)
+  
+  # add nbr bands to burn severity
+  #burnIndices = burnIndices.addBands(nbrBands)
+  
+  burnIndices = burnIndices.select('preNBR', 'postNBR', 'rbr', 'rbr_w_offset')
+  
+  export_bi_name = Name + "_bi"
+  export_nbr_name = Name + "_nbr"
+  
+  # export  burn Indices imagery
+  geemap.ee_export_image_to_drive(burnIndices, description=export_bi_name , folder="chapter3_lt_bi", region=fireBounds, scale=30)
 
-
-SWIRftvStack = swir2_fit.arrayFlatten([yearsStr]) 
-
-# water mask
-nir = water_mask(NIRftvStack)
-swir = water_mask(SWIRftvStack)
-
-#geemap.ee_export_image_to_drive(NIRftvStack, description='test_img', folder="chapter_3_landTrendr_test", region=fireBounds, scale=30)
-
-### get RBR image
-# create sequence for each fire to filter image to years we want
-start = ee.Date(fireYear.advance(-1, 'year'))
-start = getYearNumber(start)
-end = ee.Date(fireYear.advance(11, 'year'))
-end = getYearNumber(end)
-yoi = ee.List.sequence(start, end)
-yoiStr = yoi.map(getYearStr)
-yoiStr = yoiStr.getInfo()
-
-## filter image for the years we want
-nir = nir.select(yoiStr)
-swir = swir.select(yoiStr)
-
-# def function, for each year, create new img for that year wth two bands
-def mergeByYear(img1, img2, key):
-  yr = getYear(yoiStr, key)
-  nir2 = img1.select(yr).rename('nir')
-  swir2 = img2.select(yr).rename('swir')
-  merged = nir2.addBands(swir2)
-  nbr= nbrPerYear(merged)
-  if (key == 0):
-    img = merged.addBands(nbr).select('nbr').rename('preNBR')
-  elif (key == 2):
-    img = merged.addBands(nbr).select('nbr').rename('postNBR')
-  elif (key == 3):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_1yr')
-  elif (key == 4):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_2yr')
-  elif (key == 5):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_3yr')
-  elif (key == 6):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_4yr')
-  elif (key == 7):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_5yr')
-  elif (key == 8):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_6yr')
-  elif (key == 9):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_7yr')
-  elif (key == 10):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_8yr')
-  elif (key == 11):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_9yr')
-  elif (key == 12):
-    img = merged.addBands(nbr).select('nbr').rename('nbr_10yr')
-  return(img)
-
-test = mergeByYear(nir, swir, 0)
-
-geemap.ee_export_image_to_drive(test, description='preNBR_test', folder="chapter_3_landTrendr_test", region=fireBounds, scale=30)
-
-
+  # export nbr recovery imagery
+  geemap.ee_export_image_to_drive(nbrBands, description=export_nbr_name, folder="chapter3_lt_nbr", region=fireBounds, scale=30)
